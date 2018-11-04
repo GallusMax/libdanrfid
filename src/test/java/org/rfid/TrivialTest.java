0c05c8e15535c0930cf6573e6150172a0f545ecb
GallusMax <gm.10.columba@dfgh.net>
2018-11-04 17:49:39 +0100

    Create TrivialTest.java

---
+++ src/test/java/org/rfid/TrivialTest.java
@@ -0,0 +1,20 @@
+package org.rfid;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TrivialTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
