f6b162a6a6f20dacd5e1d0d389004b3423d7229e
uhahn <uhahn@WYSEMGR.library.hsu-hh.de>
2018-11-07 14:34:03 +0100

    use org.junit.Test instead of api.jupiter

--- src/test/java/org/rfid/libdanrfid/DDMDataTest.java
+++ src/test/java/org/rfid/libdanrfid/DDMDataTest.java
@@ -1,43 +1,43 @@
 package org.rfid.libdanrfid;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class DDMDataTest {
+public class DDMDataTest {
 
 	@Test
-	void testDDMDataString() {
+	public void testDDMDataString() {
 		assertNotNull(new DDMData("11010131313232333334340000000000000000513e4445373035000000000000").toString()); // just simple
 	}
 
 	@Test
-	void testToString() {
+	public void testToString() {
 		DDMData data = new DDMData();
 		assertNotNull(data.toString());
 	}
 
 	@Test
-	void testCompareCRC() {
+	public void testCompareCRC() {
 		DDMData data = new DDMData("11010131313232333334340000000000000000513e4445373035000000000000"); // just simple
 		data.toString();
 		assertEquals((int)0x3e51, data.getCRC());
 	}
 
 	@Test
-	void testGetCRC() {
+	public void testGetCRC() {
 		assertNotNull(new DDMData().getCRC());
 	}
 
 	@Test
-	void testcompareCRC() {
+	public void testcompareCRC() {
 		DDMData data = new DDMData();
 		assertNotNull(data.toString()); //sets CRC
 		assertTrue(data.compareCRC());
 	}
 	
 	@Test
-	void testIsValid() {
+	public void testIsValid() {
 		DDMData data = new DDMData("11010131313232333334340000000000000000"); // too short, no CRC
 		assertFalse(data.isvalid());
 		assertNotNull(data.toString());
@@ -45,47 +45,47 @@ class DDMDataTest {
 	}
 	
 	@Test
-	void testBarcode() {
+	public void testBarcode() {
 		DDMData data = new DDMData("11010131313232333334340000000000000000513e4445373035000000000000"); // just simple
 		String barcode = data.Barcode();
 		assertEquals("11223344",barcode);
 	}
 
 	@Test
-	void testBarcodematch() {
+	public void testBarcodematch() {
 		DDMData data = new DDMData("11010131313232333334340000000000000000513e4445373035000000000000"); // just simple
 		assertTrue(data.barcodematch("11223.+"));
 		assertFalse(data.barcodematch("abc.+"));
 	}
 
 	@Test
-	void testSetBarcode() {
+	public void testSetBarcode() {
 		DDMData data = new DDMData();
 		data.setBarcode("1g2h3j");
 		assertEquals("1g2h3j", data.Barcode());
 	}
 
 	@Test
-	void testCountry() {
+	public void testCountry() {
 		DDMData data = new DDMData();
 		assertEquals(data.myCountry, data.Country());
 	}
 
 	@Test
-	void testSetCountry() {
+	public void testSetCountry() {
 		DDMData data = new DDMData();
 		data.setCountry("XY");
 		assertEquals("XY", data.Country());
 	}
 
 	@Test
-	void testISIL() {
+	public void testISIL() {
 		DDMData data = new DDMData();
 		assertEquals(data.myISIL, data.ISIL());
 	}
 
 	@Test
-	void testSetISIL() {
+	public void testSetISIL() {
 		DDMData data = new DDMData();
 		data.setISIL("ISIL");
 		assertEquals("ISIL", data.ISIL());
