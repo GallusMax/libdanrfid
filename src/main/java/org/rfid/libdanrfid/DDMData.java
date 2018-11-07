564afd53ba44a5d2ea8a3fe66a6f8ad75f192ad1
uhahn <uhahn@WYSEMGR.library.hsu-hh.de>
2018-11-07 09:21:21 +0100

    success in pulling generic Tag functions to TagData DDMData checked ok (DM11Data still untested)

--- src/main/java/org/rfid/libdanrfid/DDMData.java
+++ src/main/java/org/rfid/libdanrfid/DDMData.java
@@ -24,13 +24,7 @@ package org.rfid.libdanrfid;
  * @author uhahn
  *
  */
-public class DDMData extends Object{
-	/*
-	 * These presets are used when a blank record is created
-	 * customize here - ugly, too!
-	 */
-	public final String myCountry="DE";
-	public final String myISIL="705";
+public class DDMData extends TagData{
 	
 	/* one byte combines Version and media usage 
 	 * this is as ugly a these constants
@@ -63,32 +57,6 @@ public class DDMData extends Object{
 
 //	public final static Hash<char,String> HStatus=new Hash<char,String>{};
 	
-	/*
-	 * TODO this schould be the real block properties from Tag
-	 */
-	protected int blocksize=4;
-	protected int nBlocks=8; // number of blocks on tag - not the DDM size!
-	protected int nDDMblocks=8; // fits into userdata32..
-	
-	/*
-	 * some like it forward, some reverse
-	 * still looking for a performant way of handling this
-	 * a cure would be a reference implementation 
-	 * 
-	 */
-	protected char[] forward32 = new char[32]; // represents the tag user fields
-//	protected char[] reverse32 = new char[32]; // reversed block order
-	protected char[] userdata32=forward32;
-	protected char[] shadowdata; // keep array as initialized for change tracking
-	/**
-	 * remember if we found reverted byteorder on tag
-	 */
-	 protected boolean reversed=false;  
-	
-	 /**
-	  * remember if the tag had a valid CRC weh we read it
-	  */
-	protected boolean foundCRCok=false;
 	/**
 	 * the DDM data format version, used on status updates
 	 */
@@ -118,50 +86,12 @@ public class DDMData extends Object{
 		System.out.format("will return the 32 byte userdata of a RFID Tag\n");
 	}
 
-	
-	/**
-	 * initialize the 32 byte user data array
-	 * @param in - a byte array as read from
-	 */
-	protected void initdata(byte[] in)
-	{
-		for (int i = 0; i < 32; i++) { // dont trust in length ;-) 
-			if(i<in.length) // any case..
-				userdata32[i]=(char)in[i];
-			else
-				userdata32[i]=(char)0; // init all
-		}
-		// is this a crc ok Record?
-		// nb: reverts the block byte order, if necessary
-		foundCRCok=isvalid();
-		
-		keepshadowdata(); // keep this state in order to track the changes		
-	}
-	
-	private void keepshadowdata(){
-		shadowdata=userdata32.clone();
-		for(int i=0;i<userdata32.length;i++)
-			shadowdata[i]=(char)(userdata32[i]&0xff); // chars may have a sign?
-	}
-	/**
-	 * compares a single byte between current array and original array
-	 * @param i
-	 * @return true if byte i was changed
-	 */
-	public boolean bytetainted(int i){
-		boolean istainted = userdata32[i]!=shadowdata[i];
-		if(istainted)
-			return istainted;
-		else
-			return istainted;
-	}
-	
 	/**
 	 * constructor from byte[] Array
 	 * @param in - userdata as bytes read from tag
 	 */
 	public DDMData(byte[] in){
-		initdata(in);
+		super(in);
 	}
 
 	/**
@@ -170,52 +100,15 @@ public class DDMData extends Object{
 	 * @return 
 	 */
 	public DDMData(Byte[] array) {
-		byte[] ret=new byte[array.length];
-		for(int i=0;i<array.length;i++){
-			ret[i]=array[i].byteValue();
-		}
-		initdata(ret);
+		super(array);
 	}
 
-	 /**
-	  * constructor from Hex String as from toString()
-	  * @param ins - 32 byte userdata as read from the tag, an optional "0x" prefix will be ignored
-	  */
+	/**
+	 * constructor from Hex String as from toString()
+	 * @param ins - 32 byte userdata as read from the tag, an optional "0x" prefix will be ignored
+	 */
 	public DDMData(String ins){
-		int startat=0;
-		ins.toLowerCase(); // ignore case
-		if(ins.startsWith("0x")){ // lets believe in HEX
-			startat=2;
-		}
-	
-		String hex=ins.substring(startat); // drop the leading 0x
-		
-		initdata(Util.hexStringToByteArray(hex));
-/*		
-		ArrayList<Byte> bal=new ArrayList<Byte>();
-		
-		  //49204c6f7665204a617661 split into two characters 49, 20, 4c...
-		  for( int i=0; i<hex.length()-1; i+=2 ){
-	 
-		      //grab the hex in pairs
-		      String output = hex.substring(i, (i + 2));
-		      //convert hex to decimal
-		      int decimal = Integer.parseInt(output, 16);
-		      //convert the decimal to character
-		      bal.add((byte)decimal);
-		  }
-		  
-	  bal.trimToSize();
-//	  DDMData((Byte[])bal.toArray());
-	  Object[] array= (bal.toArray());
-
-	  // copied helplessly
-		byte[] in=new byte[array.length];
-		for(int i=0;i<array.length;i++){
-			in[i]=((Byte)(array[i])).byteValue();
-		}
-		initdata(in);
-*/	
+		super(ins);
 	}
 	
 	/**
@@ -233,22 +126,10 @@ public class DDMData extends Object{
 	}
 
 	/**
-	 * HEX representation of the 32 byte payload of a tag
-	 * with the CRC updated
-	 */
-	public String toString(){
-		String res="";
-		updateCRC(); // the CRC is refreshed automagically before giving away the content
-		for (char d : userdata32) {
-			res=res.concat(String.format("%02x", (byte)d));			
-		}
-		return res;
-	}
-	
-	/**
 	 * rfresh the CRC at the given position
 	 * @return the int value of the new CRC 
 	 */
+	@Override
 	public int updateCRC(){
 		int crc_sum=new CRC().DDCRC(userdata32);
 
@@ -278,6 +159,7 @@ public class DDMData extends Object{
 	 * @return false if both block directions do not produce a valid CRC
 	 * the block direction will be reverted again, if both do not match
 	 */
+	@Override
 	public boolean isvalid(){
 		if(compareCRC())return true;
 		reverseblocks();
@@ -285,99 +167,6 @@ public class DDMData extends Object{
 		reverseblocks(); // back again
 		return false;
 	}
-	
-	/**
-	 * prepare single block for writing.
-	 * byteorder remains reversed, if found reversed during read
-	 * @param n - the block index
-	 * @return a byte array with the block 
-	 * TODO : with respect to ordering? ;-)
-	 */
-	public byte[] getblock(int n){
-		return getblock(n,4,false); // default to blocksize 4
-	}
-
-	/**
-	 * prepare single block for writing.
-	 * byteorder remains reversed, if found reversed during read
-	 * @param n - the block index 
-	 * @param forcereverse requests reverted byteorder (on empty tags)
-	 * @return a byte array with the block 
-	 */
-	public byte[] getblock(int n, boolean forcereverse){
-		return getblock(n,4,forcereverse); // default to blocksize 4
-	}
-
-	/**
-	 * prepare single block for writing.
-	 * byteorder remains reversed, if found reversed during read
-	 * @param n - the block index 
-	 * @param blocksize - respect tag's blocksize
-	 * @param forcereverse requests reverted byteorder (on empty tags)
-	 * @return a byte array with the block 
-	 */
-	public byte[] getblock(int n, int blocksize, boolean forcereverse){
-		byte[] res= new byte[blocksize];
-		
-		for(int i=0;i<blocksize;i++)
-			res[i]=(byte)((reversed||forcereverse) ? userdata32[(n+1)*blocksize-i-1] : userdata32[n*blocksize+i]);
-		return res;
-	}
-	
-	/**
-	 * find out if our updates have changed block n
-	 * @param n
-	 * @return true if block needs to be written back to tag
-	 */
-	public boolean blocktainted(int n){
-		return blocktainted(n,4);
-	}
-	
-	/**
-	 * find out if our updates have changed block n
-	 * @param n
-	 * @param blocksize
-	 * @return true if block needs to be written back to tag
-	 */
-	public boolean blocktainted(int n, int blocksize){
-		for(int i=0;i<blocksize;i++)
-			if(bytetainted(n*blocksize+i)) 
-				return true;
-		return false;
-	}
-	
-	/**
-	 * change the order of each block 
-	 * nb: changes order of blocks in place
-	 * property "reversed" keeps track of operations
-	 * FKI writes the blocks from 3 to 0, Biblioteca writes 0 to 3
-	 */
-	public void reverseblocks(){ // TODO use actual blocksizes from tag system data
-/*
-		int blocksize=4;
-		int numblocks=8;
-*/
-		for (int k = 0; k < nDDMblocks; k++) { // all blocks
-				reverse(userdata32,k*blocksize,blocksize); // swap string positions
-		}
-		reversed=(reversed?false:true); // toggle reversed marker
-	}
-	
-	/**
-	 * reverse a subset of an array, starting at @param offset 
-	 * changing at max @param block bytes
-	 * CHANGES THE ARRAY IN PLACE
-	 */
-	public char[] reverse(char[] in, int offset, int block){
-		char bt;
-		for (int i = 0; i < block/2; i++) {
-			bt=in[offset+i];
-			in[offset+i]=in[offset+block-i-1];
-			in[offset+block-i-1]=bt;
-		}
-		return in;
-	}
-
 	
 	/**
 	 * read the CRC as int, added up LSB first, MSB last
@@ -463,10 +252,7 @@ public class DDMData extends Object{
 	public byte getVersionUsage(){
 		return (byte)(userdata32[0]&0xff);
 	}
-	/**
-	 * extract the barcode to a string
-	 * @return
-	 */
+	@Override
 	public String Barcode(){
 		String res=new String(userdata32, 3, 16);
 		int i = res.indexOf(0);
@@ -476,59 +262,23 @@ public class DDMData extends Object{
 			return res;
 	}
 	
-	/**
-	 * does this Itemcode belong to us? 
-	 * @param barcodePattern - the pattern to be checked against
-	 * @return - true if matches, or null or empty Pattern given
-	 */
-	public boolean barcodematch(String barcodePattern){
-		if(null==barcodePattern)return true;
-		if(barcodePattern.isEmpty()) return true;
-		return Barcode().matches(barcodePattern);
-	}
-
-	
-	/**
-	 * put String @param s at postition @param start
-	 * with at max @param len characters
-	 */
-	private void setStringAt(String s,int start, int len){
-		for (int i = 0; i < len; i++) {
-			if(i>=s.length()) // well - we fill in \0 here..
-				userdata32[start+i]='\0';	
-			else
-				userdata32[start+i]=s.charAt(i);			
-		}				
-	}
-	/**
-	 * fill in the Barcode
-	 * @param bc
-	 */
+	@Override
 	public void setBarcode(String bc){
 		setStringAt(bc, 3, 16);
 	}
 	
 	
-	/**
-	 * extract the countrycode
-	 * @return
-	 */
+	@Override
 	public String Country(){
 		return new String(userdata32,21,2);
 	}
 	
-	/**
-	 * fill in the countrycode
-	 * @param iso
-	 */
+	@Override
 	public void setCountry(String iso){
 		setStringAt(iso, 21, 2);
 	}
 	
-	/**
-	 * extract the ISIL code
-	 * @return
-	 */
+	@Override
 	public String ISIL(){
 		String res= new String(userdata32,23,9); // the far end, even when ISIL allow for 11 chars
 		int i = res.indexOf(0);
@@ -536,36 +286,11 @@ public class DDMData extends Object{
 			return res.substring(0, res.indexOf(0));
 		else
 			return res;
-
 	}
-	/**
-	 * fill in the ISIL
-	 * @param iso
-	 */
+
+	@Override
 	public void setISIL(String s){
 		setStringAt(s, 23, 9);
 	}
-	
-	/**
-	 * 
-	 * @return the blocksize according to Tag system info
-	 */
-	public int getblocksize(){
-		return (int)blocksize;
-	}
-	
-	/**
-	 * 
-	 * @return the number of blocks on the Tag
-	 */
-	public int getnBlocks(){
-		return (int)nBlocks;
-	}
-
-	public void addUserData(byte[] in) {
-		initdata(in);
-	}
-	
-
 	
 }
