61559a12d0f77177ab5cefa2c7120a90fd7d4b51
GallusMax <gm.10.columba@dfgh.net>
2018-11-13 16:44:30 +0100

    Update DDMfindingsTest.java
    no jupiter.api at travis

--- src/test/java/org/rfid/libdanrfid/DDMfindingsTest.java
+++ src/test/java/org/rfid/libdanrfid/DDMfindingsTest.java
@@ -1,8 +1,8 @@
 package org.rfid.libdanrfid;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
